5e61050942fe605ba2d2f33791c918a050324051
Tjeerd in 't Veen <tjeerdintveen@Tjeerds-MacBook-Pro.local>
2018-12-17 20:04:50 +0100

    Solved 7a

--- aoc07/src/main.rs
+++ aoc07/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -8,121 +8,88 @@ fn main() {
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
     let char_map = parse(&contents);
-    print!("{}", contents);
-    print!("{:?}", char_map);
     let result = solve_first(&char_map);
-    println!("RESULT IS {:?}", result);
-    println!("Expected CABDFE");
+    println!("First result is {:?}", result);
 }
-
 
 fn solve_first(char_map: &HashMap<char, Vec<char>>) -> String {
-    // println!("char map {:?}", char_map);
-    char_map.iter().for_each(|(k, v)| {
-        println!("{} before {:?}", k, v);
-    });
-
     let mut result = String::new();
-    let mut visited = HashSet::<char>::new();
+    let mut indegrees = indegrees(&char_map);
 
-    // Start at keys
-    loop {
-        let mut keys: Vec<_> = char_map.keys().collect();
-        keys.sort_by(|lhs, rhs| rhs.cmp(lhs) ); // a-z reverse sort
-        let found = keys.iter().find(|ch| !visited.contains(&ch));
-        match found {
-            Some(ch) => {
-                visited.insert(*ch.clone());
-                visit(&ch, &char_map, &mut visited, &mut result);
-                result.push(*ch.clone());
-            },
-            None => {
-                break;
-            },
+    let roots = find_roots(&char_map, &indegrees);
+    let mut queue: String = roots.iter().collect();
+
+    while !queue.is_empty() {
+        // sort string
+        let mut chars: Vec<char> = queue.chars().collect();
+        chars.sort_by(|a, b| b.cmp(a));
+        queue = chars.iter().collect();
+
+        let popped = queue.pop().unwrap();
+        result.push(popped);
+        if let Some(neighbors) = &char_map.get(&popped) {
+            for neighbor in *neighbors {
+                let entry = indegrees.entry(*neighbor).or_insert(0);
+                *entry -= 1;
+
+                if *entry == 0 {
+                    queue.insert(0, neighbor.clone());
+                }
+            }
+        }
+    }
+    result
+}
+
+fn find_roots(char_map: &HashMap<char, Vec<char>>, indegrees: &HashMap<char, u32>) -> Vec<char> {
+    let mut roots = Vec::<char>::new();
+    char_map.keys().for_each(|k| {
+        if !&indegrees.contains_key(&k) {
+            roots.push(k.clone());
+        }
+    });
+    roots.sort_by(|lhs, rhs| rhs.cmp(lhs));
+    roots.dedup();
+    roots
+}
+
+fn indegrees(char_map: &HashMap<char, Vec<char>>) -> HashMap<char, u32> {
+    let mut indegrees = HashMap::<char, u32>::new();
+
+    for values in char_map.values() {
+        for ch in values {
+            let entry = indegrees.entry(*ch).or_insert(0);
+            *entry += 1;
         }
     }
 
-    result.chars().rev().collect()
+    indegrees
 }
-
-fn visit(ch: &char, char_map: &HashMap<char, Vec<char>>, mut visited: &mut HashSet<char>, mut acc: &mut String) {
-    let mut current_char: Option<char> = None;
-    let mut parent: char = ch.clone();
-
-    loop {
-        println!("A");
-        match char_map.get(&parent) {
-            Some(chars) => {
-                let mut sorted_chars: Vec<_> = chars.iter().collect();
-                sorted_chars.sort_by(|lhs, rhs| rhs.cmp(lhs)); // a-z reverse sort
-
-                let mut new_char: Option<char> = None;
-                for ch in sorted_chars {
-                    if !visited.contains(&ch) {
-                        new_char = Some(*ch);
-                        continue;
-                    }
-                }
-
-                current_char = new_char;
-
-                match current_char {
-                    Some(ch) => {
-                        visited.insert(ch.clone());
-                        acc.push(ch.clone());
-                        parent = ch.clone();
-                        println!("Adding {}",ch);
-                    },
-                    None => break,
-                }
-            },
-            None => {
-                println!("C");
-                return
-            },
-        }
-    }
-
-}
-
-// Recursive style.
-// fn visit(parent: &char, char_map: &HashMap<char, Vec<char>>, mut visited: &mut HashSet<char>, mut acc: &mut String) {
-//     match char_map.get(&parent) {
-//         Some(chars) => {
-//             let mut sorted_chars: Vec<_> = chars.iter().collect();
-//             sorted_chars.sort_by(|lhs, rhs| rhs.cmp(lhs)); // a-z reverse sort
-//             println!("parents {} chars {:?}", parent, chars);
-//             let mut characters = Vec::<char>::new();
-//             for ch in sorted_chars {
-//                 if visited.contains(&ch) {
-//                     continue;
-//                 }
-//                 visited.insert(ch.clone());
-//                 visit(&ch, &char_map, &mut visited, &mut acc);
-//                 characters.push(ch.clone());
-//             }
-//             println!("did insert {:?} for parent {}", &characters, &parent);
-//         },
-//         None => {
-            
-//         },
-//     }
-
-//     println!("Pushing {}", &parent);
-//     acc.push(parent.clone());
-// }
 
 fn parse(contents: &str) -> HashMap<char, Vec<char>> {
-    contents
+    let mut map = contents
         .lines()
         .map(|str| {
             (str.chars().nth(5).unwrap(), str.chars().nth(36).unwrap())
         })
-        .fold(HashMap::<char, Vec<char>>::new(), |mut char_map, (lhs, rhs)| {
-            char_map.entry(lhs).and_modify(|chars| {
-                chars.push(rhs);
-            }).or_insert(vec![rhs]);
+        .fold(
+            HashMap::<char, Vec<char>>::new(),
+            |mut char_map, (lhs, rhs)| {
+                char_map
+                    .entry(lhs)
+                    .and_modify(|chars| {
+                        chars.push(rhs);
+                    })
+                    .or_insert_with(||vec![rhs]);
 
-            char_map
-        })
+                char_map
+            },
+        );
+
+    for chars in map.values_mut() {
+        let mut sorted_chars: Vec<_> = chars.iter().collect();
+        sorted_chars.sort_by(|lhs, rhs| rhs.cmp(lhs)); // a-z reverse sort
+    }
+    map
 }
+
